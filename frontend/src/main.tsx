c1fbc99d4ad38ccd9993b7b22f9784a69c78445b
vaibhavi466 <166858629+vaibhavi466@users.noreply.github.com>
2026-09-14 18:05:02 +0530

    style: wire aesthetic theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './aesthetic-base.css'
+import './aesthetic-auth.css'
+import './aesthetic-cards.css'
+import './aesthetic-detail.css'
+import './aesthetic-analytics.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
